0387b0a299cc51f40578c44b770d7af80695e314
kenshii09 <itablada29@gmail.com>
2022-04-13 19:51:22 +0800

    Update portfolio

---
+++ myportfolio/itablada-onlineportfolio/assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#topBtn').fadeIn();
+        } else {
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $('#topBtn').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 100);
+    });
+});
